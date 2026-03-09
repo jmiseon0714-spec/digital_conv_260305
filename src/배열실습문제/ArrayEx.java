83b5438ede492b81cd32a151e83e444e9a986f93
admin <misun0714@naver.com>
2026-03-09 17:16:47 +0900

    bbbb

---
+++ src/배열실습문제/ArrayEx.java
@@ -0,0 +1,26 @@
+package 배열실습문제;
+// 알고리즘 문제풀이 > 3단계 중급 문제 > 핸드폰 요금
+// 알고리즘 문제풀이 > 3단계 중급 문제 > 저항
+
+import java.util.Scanner;
+
+public class ArrayEx {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] call = new int[20];
+
+        System.out.print(" 통화 횟수 입력 : ");
+        int n = sc.nextInt();
+        System.out.print(" 통화 시간 입력 : ");
+
+        for (int i = 0; i < n; i++) {
+            call[i] = sc.nextInt();
+        }
+        for (int i = 0; i< n; i++) {
+
+        }
+
+
+        String[] color = {"black", "brown", "red", "orange", "yellow","green", "blue", "violet", "grey", "white"};
+    }
+}
